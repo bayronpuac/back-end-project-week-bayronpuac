9cb9ef3c21e2158bac7d90e8f3c2fa532874af3d
Bayron Puac <bayronpuac@gmail.com>
2019-11-26 16:31:31 -0600

    Added the Server and the dbConfig. Working on the endpoints now.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+
+
+const authRouter = require('../auth/auth-router.js');
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/api/auth', authRouter);
+
+server.get('/', (req, res) => {
+    res.send("It's alive!");
+  });
+
+  module.exports = server;
