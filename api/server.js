2cd5339882a72ff385b69048167215ddc316710e
Bayron Puac <bayronpuac@gmail.com>
2019-11-26 16:34:52 -0600

    Added the index file before the endpoints.

--- api/server.js
+++ api/server.js
@@ -3,8 +3,6 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 
-const authRouter = require('../auth/auth-router.js');
-
 
 const server = express();
 
@@ -12,7 +10,6 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.send("It's alive!");
